7412809b69f961e7a9663a1943afc271047b3eeb
profikid <profikid@gmail.com>
2017-03-05 15:58:11 +0100

    step 4: webpack hotmodule reloading

--- src/shared/config.js
+++ src/shared/config.js
@@ -2,4 +2,10 @@
 
 export const WEB_PORT = process.env.PORT || 8000
 export const STATIC_PATH = '/static'
-export const APP_NAME = 'Dit is een bloemen app'
+export const APP_NAME = 'Tafel game'
+
+export const WDS_PORT = 7000
+
+export const APP_CONTAINER_CLASS = 'js-app'
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
+
