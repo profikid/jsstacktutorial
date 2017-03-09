55e5dcde6ee363972f6271e254ac75c67f004808
profikid <profikid@gmail.com>
2017-03-09 14:50:39 +0100

    step 5: redux implemented

---
+++ src/client/reducer/hello.js
@@ -0,0 +1,20 @@
+// @flow
+
+import * as Immutable from 'immutable'
+
+import { SAY_HELLO } from '../action/hello'
+
+const initialState = Immutable.fromJS({
+  message: 'Initial reducer message',
+})
+
+const helloReducer = (state: Object = initialState, action: { type: string, payload: any }) => {
+  switch (action.type) {
+    case SAY_HELLO:
+      return state.set('message', action.payload)
+    default:
+      return state
+  }
+}
+
+export default helloReducer
